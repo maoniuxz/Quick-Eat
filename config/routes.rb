d5b06594ce07dc4044702760573f0b94eeea17e3
ZhiyuanLi <zhil@seas.upenn.edu>
2015-04-03 17:53:05 -0300

    login system

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'pages/info'
+
+  root :to => redirect('/ideas')
   resources :ideas
   get 'welcome/index'
 
@@ -6,7 +10,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'welcome#index'
+   #root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
