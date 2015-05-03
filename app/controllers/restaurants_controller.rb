69e09108d4df4dea127d88acfcca1121615e5639
maoniuxz <zhexu1990@gmail.com>
2015-05-02 22:23:39 -0400

    new_commit

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,11 +4,9 @@ class RestaurantsController < ApplicationController
   def index
   #@restaurants = Restaurant.all
 if params[:search]
-<<<<<<< Updated upstream
+
       @restaurants = Restaurant.near(params[:search]+" Philadelphia", 0.1, order: 'distance')
-=======
-      @restaurants = Restaurant.near(params[:search], 0.3)
->>>>>>> Stashed changes
+
       #@restaurants = Restaurant.search(params[:search]).order("created_at DESC")
     else
       @restaurants = Restaurant.order("created_at DESC")
